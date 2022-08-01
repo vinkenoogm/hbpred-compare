fc3d6e8e68788545fb151d7af36488f18a3c0548
Mariekev93 <mvinkenoog@gmail.com>
2022-08-01 13:09:25 +0200

    code refactor: argparse, fstrings, pathlib

--- src/1_hyperparams.py
+++ src/1_hyperparams.py
@@ -1,29 +1,35 @@
-import numpy as np
-import pandas as pd 
-import pickle
+import argparse
 import datetime
-import sys
+from itertools import product
+from pathlib import Path
+import pickle
+import warnings
+
+import pandas as pd 
 from sklearn.svm import SVC
-from sklearn.metrics import balanced_accuracy_score, make_scorer
 from sklearn.model_selection import GridSearchCV
 from sklearn.exceptions import UndefinedMetricWarning
 
-def warn(*args, **kwargs):
-    pass
-import warnings
-warnings.warn = warn
+warnings.filterwarnings('ignore') # This suppresses the sklearn 'UndefinedMetricWarning' that occurs when one class is not predicted in a combination of hyperparameters
+
+data_path = Path('../data')
+results_path = Path('../testresults')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('nback', type=int,
+                    help='[int] number of previous Hb values to use in prediction')
+parser.add_argument('sex', type=str, choices=['men', 'women'],
+                    help='[men/women] sex to use in model')
+parser.add_argument('--foldersuffix', type=str, default='',
+                    help='[str] optional suffix indicating non-default run')
+args = parser.parse_args()
+
+nback, sex, foldersuffix = args.nback, args.sex, args.foldersuffix
 
 
-nback = int(sys.argv[1])
-sex = sys.argv[2]
-foldersuffix = sys.argv[3]
-
-print(sex)
-
-
-train = pd.read_pickle('../../data/scaled'+foldersuffix+'/'+str(sex)+'_'+str(nback)+'_train.pkl')
+train = pd.read_pickle(data_path / f'scaled{foldersuffix}/{sex}_{nback}_train.pkl')
 X = train[train.columns[:-1]]
-y = train['Hb_deferral']
+y = train['HbOK']
 
 params = {'C': [10, 1, 0.1],
           'gamma': [1, 0.1, 0.01, 0.001],
@@ -37,10 +43,13 @@ gridsearch = GridSearchCV(estimator=SVC(class_weight='balanced'),
                           verbose=2)
 gridsearch.fit(X, y)
 
-filename = '../results/hyperparams'+foldersuffix+'/output_hyperparams_' + str(sex) + '_' + str(nback) + '.pkl'
+output_folder = results_path / f'hyperparams{foldersuffix}'
+output_folder.mkdir(parents=True, exist_ok=True)  # create folder if it does not yet exist
+
+filename = output_folder / f'hyperparams_{sex}_{nback}.pkl'
+
 with open(filename, 'wb') as handle:
     pickle.dump(gridsearch.cv_results_, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open(filename, 'a') as f:
-    print(str(datetime.datetime.now()), '\n\n', gridsearch.cv_results_)
+
 

62b8c7d1a08e4dec3d0b301440f419d3bbf63340
Mariekev93 <mvinkenoog@gmail.com>
2022-08-02 12:37:03 +0200

    argparser, fstrings

--- src/4_calcshap.py
+++ src/4_calcshap.py
@@ -1,40 +1,46 @@
-import numpy as np
-import pandas as pd 
+import argparse
 import datetime
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import shap
+from pathlib import Path
 import pickle
-from sklearn.svm import SVC
-import sys
-import datetime
+import warnings
 
-import warnings 
+import pandas as pd
+import shap
+
 warnings.filterwarnings('ignore')
+data_path = Path('../data')
+results_path = Path('../testresults')
 
-sex = sys.argv[1]
-nback = sys.argv[2]
-foldersuffix = sys.argv[3]
-n = sys.argv[4]
+parser = argparse.ArgumentParser()
+parser.add_argument('nback', type=int,
+                    help='[int] number of previous Hb values to use in prediction')
+parser.add_argument('sex', type=str, choices=['men', 'women'],
+                    help='[men/women] sex to use in model')
+parser.add_argument('--n', type=int, default=100,
+                    help='[int] number of donors to calculate SHAP values on')
+parser.add_argument('--foldersuffix', type=str, default='',
+                    help='[str] optional suffix indicating non-default run')
+args = parser.parse_args()
+
+sex, nback, n, foldersuffix = args.sex, args.nback, args.n, args.foldersuffix
 
 def calc_shap(nback, sex, n=100):
-    filename = '../results/models'+foldersuffix+'/clf_' + str(nback) + '.sav'
+    filename = results_path / f'models{foldersuffix}/clf_{sex}_{nback}.sav'
     clf = pickle.load(open(filename, 'rb'))
     
-    index = 0 if sex == 'men' else 1
-    val = pd.read_pickle('../../data/scaled'+foldersuffix+'/'+str(sex)+'_'+str(nback)+'_test.pkl')
-    clf_s = clf[index]
-    X_val = val[val.columns[:-1]]
-    X_shap = shap.sample(X_val, n)
-    explainer = shap.KernelExplainer(clf_s.predict, X_shap)
-    shapvals = explainer.shap_values(X_shap, nsamples=500)
-        
-    path = '../results/shap'+foldersuffix+'/'
-    filename1 = 'Xshap_' + sex + '_' + str(nback) + '_' + str(n) + '.pkl'
-    filename2 = 'shapvals_' + sex + '_' + str(nback) + '_' + str(n) + '.pkl'
-        
-    pickle.dump(X_shap, open(path+filename1, 'wb'))
-    pickle.dump(shapvals, open(path+filename2, 'wb'))
-        
-print('Calculating shap values for ', sex, ' nback', nback, '( N =',  n, ') starting at', datetime.datetime.now())
-calc_shap(nback, sex, int(n))
+    test = pd.read_pickle(data_path / f'scaled{foldersuffix}/{sex}_{nback}_test.pkl')
+    
+    X_test = test[test.columns[:-1]]
+    X_shap = shap.sample(X_test, n)
+    explainer = shap.KernelExplainer(clf.predict, X_shap)
+    shapvals = explainer.shap_values(X_shap, nsamples=100)
+    
+    output_path = results_path / f'shap{foldersuffix}/'
+    output_path.mkdir(parents=True, exist_ok=True)
+    filename1 = f'Xshap_{sex}_{nback}_{n}.pkl'
+    filename2 = f'shapvals_{sex}_{nback}_{n}.pkl'
+
+    pickle.dump(X_shap, open(output_path / filename1, 'wb'))
+    pickle.dump(shapvals, open(output_path / filename2, 'wb'))
+
+calc_shap(nback, sex, n)
